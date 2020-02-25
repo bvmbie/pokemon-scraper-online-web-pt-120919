addd299bc6dd9c1090ea7bca079f4c5a96ef6499
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-25 16:06:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,6 +15,7 @@ attr_accessor :id, :name, :type, :db
   
   def self.find(id, db)
     found_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
+    Pokemon.new(id:, name:, type:, db:)
   end
  
 end
